d12ab279e8061bd8dbb7db5abf8d9b70e65602b8
shailesh panchal <shaileshpanchal123@gmail.com>
2024-11-19 23:26:27 +0530

    add rust function

--- samples/projects/common_program_concepts/src/main.rs
+++ samples/projects/common_program_concepts/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     variable_shadow();
     variable_scalar();
     variable_compound();
+
+    rust_function();
 }
 
 fn variable_constant() {
@@ -55,9 +57,36 @@ fn variable_compound() {
     let (first, second, third) = tuple;
 
     println!("tuple first parameter value {}", first_tuple_parameter);
-    println!("tuple second {second} and third {third}");
+    println!("tuple first {first} second {second} and third {third}");
 
     let array = [1, 2, 3, 4, 5];
 
     println!("array value {}", array[0]);
+}
+
+fn rust_function() {
+    //rust function syntex
+
+    //function syntex with input paramter
+    //fn funtion_name(variable: i32, variable_2 : u32)
+
+    //function syntext with return parameter
+
+    //note the return statemet do not have semicolon at the end of line
+    //if you add the semicolon to the end of expression , you turn it into a statement and it will not a return value.
+
+    //so in function return statement do not have semicolon at the end line
+
+    //fn function_name() -> u32
+
+    let return_value = function_return(10);
+
+    println!("the value of return_value {}", return_value);
+}
+
+fn function_return(input_parameter: u32) -> u32 {
+    println!("inside the function input paramter {}", input_parameter);
+
+    //return statement
+    input_parameter * 10
 }
